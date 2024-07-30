ba64ee88ecc11afba7d26f614236dcfb4922b1dd
X <54944183+Rossianna@users.noreply.github.com>
2024-07-30 14:57:44 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from flask import Flask, render_template, redirect, url_for, request, after_this_request, make_response
+from flask import Flask, render_template, redirect, url_for, request, make_response
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
 from flask_admin import Admin, AdminIndexView, expose, BaseView
 from models import PendingData, ApprovedData, User, Role, Permission, AdminView, db, UserView,\
@@ -35,13 +35,25 @@ def unauthorized():
     print('aaa')
     return redirect(url_for('login'))
 
+def add_csp_header(response):
+    csp = (
+        f"default-src 'self';"
+        f"script-src 'self' 'unsafe-inline';"
+        f"style-src 'self' 'unsafe-inline';"
+        f"img-src 'self';"
+    )
+    response.headers['Content-Security-Policy'] = csp
+    return response
+
 @app.route('/')
 def home():   
     blog_content = ApprovedData.query.all()
     if current_user.is_authenticated:
-        return render_template('home.html', blog_content=blog_content, current_user=current_user.username, role=current_user.role)
+        response = make_response(render_template('home.html', blog_content=blog_content, current_user=current_user.username, role=current_user.role))
+        return add_csp_header(response)
     else:
-        return render_template('home.html', blog_content=blog_content, current_user=None)
+        response = make_response(render_template('home.html', blog_content=blog_content, current_user=None))
+        return add_csp_header(response)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -52,10 +64,14 @@ def login():
         user = User.query.filter_by(username=username, password=password).first()
         if user:
             login_user(user)
-            return redirect(url_for('home'))
+            response = make_response(redirect(url_for('home')))
+            return add_csp_header(response)
         else:
-            return render_template('login.html', status='fail')
-    return render_template('login.html', status=None)
+            response = make_response(render_template('login.html', status='fail'))
+            return add_csp_header(response)
+        
+    response = make_response(render_template('login.html', status=None))
+    return add_csp_header(response)
 
 
 @app.route('/post_a_blog', methods=['POST', 'GET'])
@@ -67,15 +83,18 @@ def post_a_blog():
             blog_index = request.form.get('blog_index')
             blog = PendingData.query.get(blog_index)
             blog_content = blog.content
-            return render_template('post_a_blog.html', blog_index=blog_index, blog_content=blog_content)
+            response = make_response(render_template('post_a_blog.html', blog_index=blog_index, blog_content=blog_content))
+            return add_csp_header(response)
     elif request.method == 'GET':
         blog_index = request.args.get('blog_index')
         if blog_index is not None:
             blog = PendingData.query.get(blog_index)
             blog_content = blog.content
-            return render_template('post_a_blog.html', blog_index=blog_index, blog_content=blog_content, role=current_user.role)
+            response = make_response(render_template('post_a_blog.html', blog_index=blog_index, blog_content=blog_content, role=current_user.role))
+            return add_csp_header(response)
         else:
-            return render_template('post_a_blog.html', blog_index=None, role=current_user.role)
+            response = make_response(render_template('post_a_blog.html', blog_index=None, role=current_user.role))
+            return add_csp_header(response)
 
 
 @app.route('/edit_a_blog', methods=['POST'])
@@ -87,28 +106,30 @@ def edit_a_blog():
         pending_data = PendingData(username=current_user.username, content=blog_content)
         db.session.add(pending_data)
         db.session.commit()
-        return redirect(url_for('home'))
+        response = make_response(redirect(url_for('home')))
+        return add_csp_header(response)
     else:
         blog_content = request.form.get('blog_content')
         blog = PendingData.query.get(blog_index)
         blog.content = blog_content
         db.session.commit()
-    return 'success'
+    return make_response('success')
 
 
 @app.route('/approve_a_blog')
 @login_required
 def approve_a_blog():
     blog_content = PendingData.query.all()
-    return render_template('approve.html', blog_content=blog_content, current_user=current_user.username)
+    response = make_response(render_template('approve.html', blog_content=blog_content, current_user=current_user.username))
+    return add_csp_header(response)
 
 
 @app.route('/review_a_blog')
 @login_required
 def review_a_blog():
     blog_content = PendingData.query.filter_by(username=current_user.username).all()
-    return render_template('review.html', blog_content=blog_content, current_user=current_user.username)
-
+    response = make_response(render_template('review.html', blog_content=blog_content, current_user=current_user.username))
+    return add_csp_header(response)
 
 @app.route('/approve', methods=['POST'])
 @login_required
@@ -122,13 +143,14 @@ def approve():
     print(pending_data)
     db.session.delete(pending_data)
     db.session.commit()
-    return 'success'
+    return make_response('success')
 
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    response = make_response(redirect(url_for('home')))
+    return add_csp_header(response)
 
 
 if __name__ == '__main__':
